f16981b334197541c9e6de6cba86132af21e85f2
paras-ameri100 <paras.bhatt@ameri100.com>
2023-06-01 18:34:28 +0530

    Bug fix

--- src/components/layout/AppLayout.tsx
+++ src/components/layout/AppLayout.tsx
@@ -18,7 +18,7 @@ export default function AppLayout() {
         </div>
         <div className='page-content px-8 py-8'>
           <Routes>
-            <Route element={<Contact />} />
+            <Route path="/" element={<Contact />} />
             <Route path="/charts-and-maps" element={<ChartsMaps />} />
             
           </Routes>
